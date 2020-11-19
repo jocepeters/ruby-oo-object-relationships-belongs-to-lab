ba72fde932a3fc91401de213d3b1875be3090b6f
Jocelyn Peters <jnutter1990@gmail.com>
2020-11-18 21:53:39 -0500

    commit tree 9e061ebc66e0a270173b42ef15204fc684ac2acf

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+
+    attr_accessor :title, :author
+
+  #  def initialize(title, author)
+  #      @title = title
+  #      @author = author
+  #  end
+
+   # hello_world = Post.new ("Hello World", "Uncle Bob")
+
+    
+end
